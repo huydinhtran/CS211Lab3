ec9c6bf9271860315be3a6aeb7cf6c90ac63a6cb
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-11-10 19:53:10 -0800

    Update sieve1.c

--- sieve1.c
+++ sieve1.c
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]) {
       }
       if (p > 1) MPI_Bcast(&prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
    } while (prime * prime <= n);
-   count = 1;
+   count = 0;
    for (i = 0; i < size; i++)
       if (!marked[i]) count++;
    if (p > 1)
