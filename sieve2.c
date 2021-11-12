b4dcd867b464e9a4890973b3b19f546d07792af9
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-11-12 15:05:42 -0800

    Update sieve2.c

--- sieve2.c
+++ sieve2.c
@@ -83,7 +83,7 @@ int main (int argc, char *argv[])
       exit(1);
    }
    
-   sqrt_n = sqrt(n);
+   int sqrt_n = sqrt(n);
    local_prime_marked = (char*)calloc(sqrt_n + 1, 1);
    for (i = 2; i <= sqrt_n; i += 2){
       local_prime_marked[i] = 1;
