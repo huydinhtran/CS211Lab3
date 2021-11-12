e20fa62004035b3d7daafdfd7d4b58d270134b00
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-11-12 15:04:43 -0800

    Update sieve2.c

--- sieve2.c
+++ sieve2.c
@@ -14,25 +14,25 @@
 
 int main (int argc, char *argv[])
 {
-   unsigned long int      count;            /* Local prime count */
-   double                 elapsed_time;     /* Parallel execution time */
-   unsigned long int      first;            /* Index of first multiple */
-   int                    local_first;
-   unsigned long int      global_count = 0; /* Global prime count */
-   unsigned long long int high_value;       /* Highest value on this proc */
+   unsigned long int      count;             /* Local prime count */
+   double                 elapsed_time;      /* Parallel execution time */
+   unsigned long int      first;             /* Index of first multiple */
+   int                    local_first;       /* New */
+   unsigned long int      global_count = 0;  /* Global prime count */
+   unsigned long long int high_value;        /* Highest value on this proc */
    unsigned long int      i;
-   int                    id;               /* Process ID number */
-   unsigned long int      index;            /* Index of current prime */
-   unsigned long long int low_value;        /* Lowest value on this proc */
-   char                  *marked;           /* Portion of 2,...,'n' */
-   char                  *local_prime_marked;
-   unsigned long long int n;                /* Sieving from 2, ..., 'n' */
-   int                    p;                /* Number of processes */
-   unsigned long int      proc0_size;       /* Size of proc 0's subarray */
+   int                    id;                /* Process ID number */
+   unsigned long int      index;             /* Index of current prime */
+   unsigned long long int low_value;         /* Lowest value on this proc */
+   char                  *marked;            /* Portion of 2,...,'n' */
+   char                  *local_prime_marked;/* New */
+   unsigned long long int n;                 /* Sieving from 2, ..., 'n' */
+   int                    p;                 /* Number of processes */
+   unsigned long int      proc0_size;        /* Size of proc 0's subarray */
    unsigned long int      prime;
-   unsigned long int      local_prime;      /* Current prime */
-   unsigned long int      size;             /* Elements in 'marked' */
-   unsigned long int      local_prime_size;
+   unsigned long int      local_prime;       /* Current prime NEW */
+   unsigned long int      size;              /* Elements in 'marked' */
+   unsigned long int      local_prime_size;  /* New */
 
 
    MPI_Init (&argc, &argv);
@@ -81,6 +81,19 @@ int main (int argc, char *argv[])
       printf("Cannot allocate enough memory\n");
       MPI_Finalize();
       exit(1);
+   }
+   
+   sqrt_n = sqrt(n);
+   local_prime_marked = (char*)calloc(sqrt_n + 1, 1);
+   for (i = 2; i <= sqrt_n; i += 2){
+      local_prime_marked[i] = 1;
+   } 
+
+   for (prime = 3; prime <= sqrt_n; prime += 2){
+      if (local_prime_marked[prime] == 1) continue;      
+      for (i = prime << 1; i <= sqrt_n; i += prime){
+      local_prime_marked[i] = 1;
+      }
    }
 
    for (i = 0; i < size; i++) marked[i] = 0;
