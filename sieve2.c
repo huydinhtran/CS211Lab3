786f8aac03bca95e47dbb8e09715f14315715ecf
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-11-14 15:17:31 -0800

    Update sieve2.c

--- sieve2.c
+++ sieve2.c
@@ -68,11 +68,11 @@ int main (int argc, char *argv[])
       exit(1);
    }
 /////////////////////////////Sieve2/////////////////////////////////////////////////////////// 
-//    local_prime_marked = (char*)malloc(sqrt(n)+1);
+   local_prime_marked = (char*)malloc(sqrt(n)+1);
 
-//    for (i = 2; i <= sqrt(n); i += prime){
-//       local_prime_marked[i] = 1;
-//    } 
+   for (i = 2; i <= sqrt(n); i += prime){
+      local_prime_marked[i] = 1;
+   } 
 
 //    for (prime = 3; prime*prime <= sqrt(n); prime += 2){
 //       if (local_prime_marked[prime] == 1)
