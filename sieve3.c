d50b0a371afee316fe047a2b76bca4222006d748
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-11-15 14:19:24 -0800

    Update sieve3.c

--- sieve3.c
+++ sieve3.c
@@ -135,7 +135,7 @@ int main (int argc, char *argv[])
    count = 0;
    for (i = 0; i < size; i++)
       if (!marked[i]) count++;
-   // if (p > 1)
+   if (p > 1)
       MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
    /* Stop the timer */
