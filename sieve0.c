621c70dd2efb1b8397039bdaf33f42cf6a7d2652
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2021-11-15 18:34:38 -0800

    Update sieve0.c

--- sieve0.c
+++ sieve0.c
@@ -4,37 +4,42 @@
 #include <stdlib.h>
 #define MIN(a,b)  ((a)<(b)?(a):(b))
 
-int main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
-   unsigned long int      count;            /* Local prime count */
-   double                 elapsed_time;     /* Parallel execution time */
-   unsigned long int      first;            /* Index of first multiple */
-   unsigned long int      global_count = 2; /* Global prime count */
-   unsigned long long int high_value;       /* Highest value on this proc */
+   unsigned long int      count;             /* Local prime count */
+   double                 elapsed_time;      /* Parallel execution time */
+   unsigned long int      first;             /* Index of first multiple */
+   int                    local_first;       
+   unsigned long int      global_count = 0;  /* Global prime count */
+   unsigned long long int high_value;        /* Highest value on this proc */
    unsigned long int      i;
-   int                    id;               /* Process ID number */
-   unsigned long int      index;            /* Index of current prime */
-   unsigned long long int low_value;        /* Lowest value on this proc */
-   char                  *marked;           /* Portion of 2,...,'n' */
-   unsigned long long int n;                /* Sieving from 2, ..., 'n' */
-   int                    p;                /* Number of processes */
-   unsigned long int      proc0_size;       /* Size of proc 0's subarray */
-   unsigned long int      prime;            /* Current prime */
-   unsigned long int      size;             /* Elements in 'marked' */
+   int                    id;                /* Process ID number */
+   unsigned long int      index;             /* Index of current prime */
+   unsigned long long int low_value;         /* Lowest value on this proc */
+   char                  *marked;            /* Portion of 2,...,'n' */
+   char                  *local_prime_marked;
+   unsigned long long int n;                 /* Sieving from 2, ..., 'n' */
+   int                    p;                 /* Number of processes */
+   unsigned long int      proc0_size;        /* Size of proc 0's subarray */
+   unsigned long int      prime;
+   unsigned long int      local_prime;       /* Current prime */
+   unsigned long int      size;              /* Elements in 'marked' */
+   unsigned long int      local_prime_size;  
 
-   MPI_Init(&argc, &argv);
+
+   MPI_Init (&argc, &argv);
 
    /* Start the timer */
 
-   MPI_Comm_rank(MPI_COMM_WORLD, &id);
-   MPI_Comm_size(MPI_COMM_WORLD, &p);
+   MPI_Comm_rank (MPI_COMM_WORLD, &id);
+   MPI_Comm_size (MPI_COMM_WORLD, &p);
    MPI_Barrier(MPI_COMM_WORLD);
    elapsed_time = -MPI_Wtime();
 
    if (argc != 2) {
-      if (!id) printf("Command line: %s <m>\n", argv[0]);
+      if (!id) printf ("Command line: %s <m>\n", argv[0]);
       MPI_Finalize();
-      exit(1);
+      exit (1);
    }
 
    n = atoll(argv[1]);
@@ -42,12 +47,18 @@ int main (int argc, char *argv[])
    /* Figure out this process's share of the array, as
       well as the integers represented by the first and
       last array elements */
+
+   /* Add you code here  */
+   /* Figure out this process's share of the array, as
+      well as the integers represented by the first and
+      last array elements */   
    unsigned long long int m = (n-1)/2;
    unsigned long long int low_index  = id * m / p;
    unsigned long long int high_index = (id+1) * m / p - 1;
    low_value  = 2 * low_index + 3;
    high_value = 2 * high_index + 3;
    size = (high_value - low_value)/2 + 1;
+   
 
    proc0_size = (n - 1) / p;
 
@@ -56,7 +67,26 @@ int main (int argc, char *argv[])
       MPI_Finalize();
       exit(1);
    }
+/////////////////////////////Sieve2/////////////////////////////////////////////////////////// 
+   local_prime_marked = (char*)malloc(sqrt(n)+1);
+   
+   for (i = 0; i <= sqrt(n); i++){
+      local_prime_marked[i] = 0;
+   } 
+   
+   for (i = 2; i <= sqrt(n); i += 2){
+      local_prime_marked[i] = 1;
+   } 
 
+   for (prime = 3; prime*prime <= sqrt(n); prime += 2){
+      if (local_prime_marked[prime] == 1)
+         continue;
+
+      for (i = prime*prime; i <= sqrt(n); i += prime){
+         local_prime_marked[i] = 1;
+      }
+   } 
+/////////////////////////////Sieve1///////////////////////////////////////////////////////////
    marked = (char *) malloc(size);
 
    if (marked == NULL) {
@@ -66,7 +96,6 @@ int main (int argc, char *argv[])
    }
 
    for (i = 0; i < size; i++) marked[i] = 0;
-   if (!id) index = 0;  
    prime = 3;
    do {
       if (prime * prime > low_value)
@@ -77,18 +106,15 @@ int main (int argc, char *argv[])
          else first =  (2*prime - (low_value % prime))/2;
       }
       for (i = first; i < size; i += prime) marked[i] = 1;
-      if (!id) {
-         while (marked[++index]);
-         prime = index*2 + 3;
-      }
-      if (p > 1) MPI_Bcast(&prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
-   } while (prime * prime <= n);
+      do {
+         prime += 2;
+      } while(local_prime_marked[prime] && prime <= sqrt(n)); 
+   } while (prime * prime <= n);      
    count = 0;
    for (i = 0; i < size; i++)
       if (!marked[i]) count++;
    if (p > 1)
-      MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM,
-                  0, MPI_COMM_WORLD);
+      MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
    /* Stop the timer */
 
@@ -101,7 +127,6 @@ int main (int argc, char *argv[])
       global_count += 1;
       printf("The total number of prime: %ld, total time: %10.6f, total node %d\n", global_count, elapsed_time, p);
    }
-   MPI_Finalize();
+   MPI_Finalize ();
    return 0;
-
 }
